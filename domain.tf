e8f0bae2a4ba353b3c639044be6bea35519b6103
Igor Rodionov <goruha@gmail.com>
2018-07-03 18:47:36 +0600

    Added ses forwarder

---
+++ domain.tf
@@ -0,0 +1,41 @@
+data "aws_route53_zone" "default" {
+  name    = "${var.domain}"
+  comment = "Root DNS Zone"
+}
+
+resource "aws_route53_record" "default" {
+  zone_id = "${data.aws_route53_zone.default.zone_id}"
+  name    = "_amazonses.${aws_ses_domain_identity.default.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["${aws_ses_domain_identity.default.verification_token}"]
+}
+
+# Example Route53 MX record
+resource "aws_route53_record" "mx" {
+  zone_id = "${data.aws_route53_zone.default.zone_id}"
+  name    = "${aws_ses_domain_identity.default.id}"
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 inbound-smtp.${var.region}.amazonaws.com"]
+  # Change to the region in which `aws_ses_domain_identity.example` is created
+}
+
+# Example Route53 TXT record for SPF
+resource "aws_route53_record" "txt" {
+  zone_id = "${data.aws_route53_zone.default.zone_id}"
+  name    = "${aws_ses_domain_identity.default.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_ses_domain_identity" "default" {
+  domain = "${var.domain}"
+}
+
+resource "aws_ses_domain_identity_verification" "default" {
+  domain = "${aws_ses_domain_identity.default.id}"
+
+  depends_on = ["aws_route53_record.default"]
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Application or solution name (e.g. `app`)"
+  default     = "ses"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "gateway_email" {}
+
+variable "emails" {
+  type = "map"
+  default = {
+    "ops@example.com" = ["destination@example.com"]
+  }
+  description = "Emails forward map"
+}
+
+variable "domain" {
+  type        = "string"
+  description = "Root domain name"
+}
+
+variable "region" {
+  type        = "string"
+  description = "AWS Region the SES should reside in"
+  default     = "us-west-2"
+}
+
