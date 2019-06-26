36be3ae7b1477f360d67ba1d72768a014dcdb7fd
rverma-nikiai <rohit.verma@niki.ai>
2019-06-26 18:10:57 +0530

    upgraded 0.12

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.1"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
