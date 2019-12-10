bf9f01fbe47068422994c8e70379b284fc273c3d
aknysh <andriy.knysh@gmail.com>
2019-12-10 16:47:59 -0500

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -24,13 +24,48 @@ func TestExamplesComplete(t *testing.T) {
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
-	//// Run `terraform output` to get the value of an output variable
-	//file := terraform.Output(t, terraformOptions, "artifact_file")
-	//// Verify we're getting back the outputs we expect
-	//assert.Equal(t, "./lambda.zip", file)
+	// Run `terraform output` to get the value of an output variable
+	artifactFile := terraform.Output(t, terraformOptions, "artifact_file")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "../../lambda.zip", artifactFile)
 
 	// Run `terraform output` to get the value of an output variable
-	url := terraform.Output(t, terraformOptions, "artifact_url")
+	artifactUrl := terraform.Output(t, terraformOptions, "artifact_url")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, "https://artifacts.cloudposse.com/terraform-external-module-artifact/example/test.zip", url)
+	assert.Equal(t, "https://artifacts.cloudposse.com/terraform-external-module-artifact/example/test.zip", artifactUrl)
+
+	// Run `terraform output` to get the value of an output variable
+	lambdaFunctionArn := terraform.Output(t, terraformOptions, "lambda_function_arn")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, lambdaFunctionArn, "function:eg-test-lambda-forwarder-test")
+
+	// Run `terraform output` to get the value of an output variable
+	lambdaIamPolicyArn := terraform.Output(t, terraformOptions, "lambda_iam_policy_arn")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, lambdaIamPolicyArn, "policy/eg-test-lambda-forwarder-test")
+
+	// Run `terraform output` to get the value of an output variable
+	lambdaIamPolicyName := terraform.Output(t, terraformOptions, "lambda_iam_policy_name")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "eg-test-lambda-forwarder-test", lambdaIamPolicyName)
+
+	// Run `terraform output` to get the value of an output variable
+	s3BucketArn := terraform.Output(t, terraformOptions, "s3_bucket_arn")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "arn:aws:s3:::eg-test-lambda-forwarder-test", s3BucketArn)
+
+	// Run `terraform output` to get the value of an output variable
+	sesDomainIdentityArn := terraform.Output(t, terraformOptions, "ses_domain_identity_arn")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, sesDomainIdentityArn, "identity/testing.cloudposse.co")
+
+	// Run `terraform output` to get the value of an output variable
+	sesReceiptRuleName := terraform.Output(t, terraformOptions, "ses_receipt_rule_name")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "eg-test-lambda-forwarder-test", sesReceiptRuleName)
+
+	// Run `terraform output` to get the value of an output variable
+	sesReceiptRuleSetName := terraform.Output(t, terraformOptions, "ses_receipt_rule_set_name")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "eg-test-lambda-forwarder-test", sesReceiptRuleSetName)
 }
