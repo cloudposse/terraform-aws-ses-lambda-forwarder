4a0bf27eca451144c5ada3c6edc39cb62a484a2f
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-20 11:22:47 -0400

    Update versions.tf

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -6,17 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 2.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = ">= 1.2"
-    }
   }
 }
